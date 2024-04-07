13700c3bab2002d0d2f2f1696895080c17b6741b
Iqra Shaheen <iqrashaheen326@gmail.com>
2024-04-07 12:25:19 +0500

    redux persist

---
+++ src/redux/store.ts
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterSlice);
+
+export const store = configureStore({
+  reducer: {
+    counter: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
